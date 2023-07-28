3da057deec7057a02ea157721f3db55772e01b81
alagesan <82628512+alagesan007@users.noreply.github.com>
2023-07-28 16:52:28 +0530

    Add files via upload

---
+++ OppsUi/index.js
@@ -0,0 +1,18 @@
+// class View{
+//     constructor(name,code){
+//         this.name = name
+//         this.code = code
+//     }
+    
+
+// }
+
+function handling(){
+    alert("handling function")
+    var name = document.getElementById('name').value
+    var code = document.getElementById('code').value
+
+    var show = document.getElementById('showed')
+    show.innerHTML = name
+        
+}
